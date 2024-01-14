0efe75134b501bef5f216a4832e176645e09cbb4
A-R-S-L-A-N_M-D <134489993+Arsalan010officiall@users.noreply.github.com>
2024-01-14 08:15:23 -0800

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -1,13 +1,20 @@
+const { 
+    Module, 
+    isPublic, 
+    getJson, 
+    sleep, 
+    tiny, 
+    webp2mp4, 
+    toAudio
+} = require("../lib/");
 const config = require("../config");
-const { Module, isPublic, getJson, sleep, tiny, webp2mp4, toAudio } = require("../lib/");
-const { jslbuffer } = require('abu-bot');
 const { Image } = require("node-webpmux");
 
 Module(
   {
     pattern: "sticker",
     fromMe: isPublic,
-    desc: "converts Photo or video to stickers",
+    desc: "converts photo or video to stickers",
     type: "converter",
   },
   async (message, match, m) => {
@@ -29,7 +36,7 @@ Module({
           type: 'converter'}, 
           async (m, match) => {
 match = match || m.reply_message.text
-if (!match) return await m.reply("Give me a text to reverse")
+if (!match) return await m.reply("_Give me a text to reverse_")
 await m.reply(match.split("").reverse().join(""))
 });
 
@@ -41,7 +48,7 @@ Module({
          
           async (m, match, client) => {
 match = match || m.reply_message.text
-if(!m.reply_message.text) return await m.reply("Give me text to encode")
+if(!m.reply_message.text) return await m.reply("_Give me text to encode_")
 await m.reply(btoa(match));
 });
 
@@ -53,7 +60,7 @@ Module({
          
          async (m, match, client) => {
 match = match || m.reply_message.text
-if(!m.reply_message.text) return await m.reply("Give me text to encode")
+if(!m.reply_message.text) return await m.reply("_Give me text to encode_")
 await m.reply(atob(match));
 });
 
@@ -65,7 +72,7 @@ Module({
          
          async (m, match, client) => {
 match = match || m.reply_message.text
-if(!m.reply_message.text) return await m.reply("Give me text to encode")
+if(!m.reply_message.text) return await m.reply("_Give me text to encode_")
 await m.reply(Buffer.from(match, 'utf8').toString('hex'));
 });
 
@@ -77,52 +84,9 @@ Module({
          
          async (m, match, client) => {
 match = match || m.reply_message.text
-if(!m.reply_message.text) return await m.reply("Give me text to encode")
+if(!m.reply_message.text) return await m.reply("_Give me text to encode_")
 await m.reply(Buffer.from(match, 'hex').toString());
 });
-
-
-
-Module(
-  {
-    pattern: "tgs",
-    fromMe: isPublic,
-    desc: "download Sticker From Telegram",
-    type: "downloader",
-  },
-  async (message, match) => {
-    if (!match)
-      return message.reply(
-        "_Enter a tg sticker url_\nEg: https://t.me/addstickers/Oldboyfinal\nKeep in mind that there is a chance of ban if used frequently"
-      );
-    let packid = match.split("/addstickers/")[1];
-    let { result } = await getJson(
-      `https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(
-        packid
-      )}`
-    );
-    if (result.is_animated)
-      return message.reply("_Animated stickers are not supported_");
-    message.reply(
-      `*Total stickers :* ${result.stickers.length}\n*Estimated complete in:* ${
-        result.stickers.length * 1.5
-      } seconds`.trim()
-    );
-    for (let sticker of result.stickers) {
-      let file_path = await getJson(
-        `https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${sticker.file_id}`
-      );
-      await message.sendMessage(
-        `https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/${file_path.result.file_path}`,
-        { packname: message.pushName, quoted: message },
-        "sticker"
-      );
-      sleep(1500);
-    }
-  }
-);
-
-
 
 Module(
   {
@@ -215,34 +179,5 @@ Module(
   }
 );
 
-Module({
-	pattern: 'ttp ?(.*)',
-	fromMe: isPublic,
-	desc: 'converter your text to ttp sticker',
-	type: 'converter',
-}, async (message, match) => {
-    if (!match) return await message.reply("_need word_");
-const Jsl_0x152d66=Jsl_0x290a;function Jsl_0x290a(_0xac6923,_0x2fd089){const _0x5dbc96=Jsl_0x5dbc();return Jsl_0x290a=function(_0x290ac9,_0x1984cb){_0x290ac9=_0x290ac9-0x19f;let _0xac969c=_0x5dbc96[_0x290ac9];return _0xac969c;},Jsl_0x290a(_0xac6923,_0x2fd089);}function Jsl_0x5dbc(){const _0x101caf=['192398qavkoD','27fNTFVR','9dsqciY','75Kethjb','95132cpJKTv','14753940sovovx','14KgbyZr','https://inrl-web.onrender.com/api/attp?text=','4009032fPsIMe','3783272Cdohqm','957836PEkOMH','2105257lQqIuI','120XtxbmD'];Jsl_0x5dbc=function(){return _0x101caf;};return Jsl_0x5dbc();}(function(_0x557715,_0x50aa0e){const _0x4028a4=Jsl_0x290a,_0x208397=_0x557715();while(!![]){try{const _0xb3b14d=-parseInt(_0x4028a4(0x1a8))/0x1+parseInt(_0x4028a4(0x1ab))/0x2*(parseInt(_0x4028a4(0x19f))/0x3)+-parseInt(_0x4028a4(0x1a2))/0x4*(parseInt(_0x4028a4(0x1a1))/0x5)+parseInt(_0x4028a4(0x1a6))/0x6*(parseInt(_0x4028a4(0x1a4))/0x7)+parseInt(_0x4028a4(0x1a7))/0x8*(-parseInt(_0x4028a4(0x1a0))/0x9)+-parseInt(_0x4028a4(0x1a3))/0xa+-parseInt(_0x4028a4(0x1a9))/0xb*(-parseInt(_0x4028a4(0x1aa))/0xc);if(_0xb3b14d===_0x50aa0e)break;else _0x208397['push'](_0x208397['shift']());}catch(_0xc8655f){_0x208397['push'](_0x208397['shift']());}}}(Jsl_0x5dbc,0xd0481));let a=await jslbuffer(Jsl_0x152d66(0x1a5)+match);
- return message.sendMessage(a,{packname:'👀',
-        quoted: message,
-      },"sticker") 
-         }
-     )     
-     
-     
-Module({
-	pattern: 'attp ?(.*)',
-	fromMe: isPublic,
-	desc: 'convert your text to attp sticker',
-	type: 'converter',
-}, async (message, match) => {
-    if (!match) return await message.reply("_Example:ttp Abu_");    
-const Jsl_0x152d66=Jsl_0x290a;function Jsl_0x290a(_0xac6923,_0x2fd089){const _0x5dbc96=Jsl_0x5dbc();return Jsl_0x290a=function(_0x290ac9,_0x1984cb){_0x290ac9=_0x290ac9-0x19f;let _0xac969c=_0x5dbc96[_0x290ac9];return _0xac969c;},Jsl_0x290a(_0xac6923,_0x2fd089);}function Jsl_0x5dbc(){const _0x101caf=['192398qavkoD','27fNTFVR','9dsqciY','75Kethjb','95132cpJKTv','14753940sovovx','14KgbyZr','https://inrl-web.onrender.com/api/attp?text=','4009032fPsIMe','3783272Cdohqm','957836PEkOMH','2105257lQqIuI','120XtxbmD'];Jsl_0x5dbc=function(){return _0x101caf;};return Jsl_0x5dbc();}(function(_0x557715,_0x50aa0e){const _0x4028a4=Jsl_0x290a,_0x208397=_0x557715();while(!![]){try{const _0xb3b14d=-parseInt(_0x4028a4(0x1a8))/0x1+parseInt(_0x4028a4(0x1ab))/0x2*(parseInt(_0x4028a4(0x19f))/0x3)+-parseInt(_0x4028a4(0x1a2))/0x4*(parseInt(_0x4028a4(0x1a1))/0x5)+parseInt(_0x4028a4(0x1a6))/0x6*(parseInt(_0x4028a4(0x1a4))/0x7)+parseInt(_0x4028a4(0x1a7))/0x8*(-parseInt(_0x4028a4(0x1a0))/0x9)+-parseInt(_0x4028a4(0x1a3))/0xa+-parseInt(_0x4028a4(0x1a9))/0xb*(-parseInt(_0x4028a4(0x1aa))/0xc);if(_0xb3b14d===_0x50aa0e)break;else _0x208397['push'](_0x208397['shift']());}catch(_0xc8655f){_0x208397['push'](_0x208397['shift']());}}}(Jsl_0x5dbc,0xd0481));let a=await jslbuffer(Jsl_0x152d66(0x1a5)+match);
-return message.sendMessage(a,{packname:'👀',
-        quoted: message,
-      },"sticker") 
-         }
-     )     
-     
-	    
-	 
+
+					    
